1326fe2c23349ae1376d844a47330f1533eccd00
Ankit Salian <ankitsalian9@gmail.com>
2020-10-18 08:29:28 +0530

    Added custom validation pipeline

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipes';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -9,7 +10,7 @@ export class TasksController {
     constructor(private tasksService: TasksService) { }
 
     @Get()
-    getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Task[] {
         if (Object.keys(filterDto).length) {
             return this.tasksService.getTaskWithFilters(filterDto);
         }
@@ -36,7 +37,7 @@ export class TasksController {
     @Patch('/:id/status')
     updateTask(
         @Param('id') id: string,
-        @Body('status') status
+        @Body('status', TaskStatusValidationPipe) status
     ): Task {
         return this.tasksService.updateTask(id, status);
     }

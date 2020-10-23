9bb1ca2c37bd1c988cab6643d2db3a1f1983b61e
Ankit Salian <ankitsalian9@gmail.com>
2020-10-23 15:23:01 +0530

    Configured Password verification at signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,10 +7,15 @@ export class AuthController {
 
     constructor(
         private authService: AuthService,
-    ) {}
+    ) { }
 
     @Post('/signup')
-    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void>{
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authCredentialsDto);
+    }
+
+    @Post('/signin')
+    signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        return this.authService.signIn(authCredentialsDto);
     }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,6 @@
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
+import * as bcrypt from "bcrypt";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
 
@@ -9,26 +10,40 @@ export class UserRepository extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
-        const exists = this.findOne({ username });
+        const salt = await bcrypt.genSalt();
 
-        if (exists) {
-
-        }
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.password = await this.hashPassword(password, salt);
+        user.salt = salt;
 
         try {
             await user.save();
         } catch (error) {
-            
+
             if (parseInt(error.code) === 23505) {
                 throw new ConflictException(`User with name "${username}" already exists`);
             } else {
                 throw new InternalServerErrorException();
             }
-            
+
         }
 
+    }
+
+    async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+
+        const user = await this.findOne({ username });
+
+        if (user && await user.validatePassword(password)) {
+            return user.username;
+        } else {
+            return null;
+        }
+    }
+
+    private async hashPassword(password: string, salt: string) {
+        return bcrypt.hash(password, salt);
     }
 }

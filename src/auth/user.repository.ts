ac5af8b0f0198a8410e030470e82255b47c0b2c5
Ankit Salian <ankitsalian9@gmail.com>
2020-10-23 14:15:04 +0530

    Exception for duplicate user configured

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
@@ -7,10 +8,27 @@ export class UserRepository extends Repository<User> {
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
+
+        const exists = this.findOne({ username });
+
+        if (exists) {
+
+        }
         const user = new User();
         user.username = username;
         user.password = password;
 
-        await user.save();
+        try {
+            await user.save();
+        } catch (error) {
+            
+            if (parseInt(error.code) === 23505) {
+                throw new ConflictException(`User with name "${username}" already exists`);
+            } else {
+                throw new InternalServerErrorException();
+            }
+            
+        }
+
     }
 }
